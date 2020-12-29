a3b8bb240ccea956e3671d0a20fbf790ef83ff49
Oliwia Rutkowska <68566596+Oliwia789@users.noreply.github.com>
2020-12-29 17:55:17 +0100

    Book informations

---
+++ P2_Book/Book.py
@@ -0,0 +1,13 @@
+class Book:
+
+    def __init__(self, url, productCode, title, priceTTC, priceHT, availableCount, description, reviewRating, imageUrl):
+        self.url = url
+        self.productCode = productCode
+        self.title = title
+        self.priceTTC = priceTTC
+        self.priceHT = priceHT
+        self.availableCount = availableCount
+        self.description = description
+        self.reviewRating = reviewRating
+        self.imageUrl = imageUrl
+        
